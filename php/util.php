6363c15a78c29cf6da7dd324d784e35a32432713
momotaro1105 <kenta.aratani5011@gmail.com>
2022-05-31 22:53:22 +0900

    random

--- php/util.php
+++ php/util.php
@@ -2,4 +2,8 @@
     function console_log($input){
         echo "<script>console.log(".json_encode($input).")</script>";
     }
+
+    function h($s){
+        return htmlspecialchars($s, ENT_QUOTES);
+    }
 ?>
