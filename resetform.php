0f6d81889613827dc0299411676de57acbbb71b3
momotaro1105 <kenta.aratani5011@gmail.com>
2022-06-08 18:22:28 +0900

    phpmailer

--- resetform.php
+++ resetform.php
@@ -1,31 +1,51 @@
 <?php
-    include("php/header.php");
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/SMTP.php';
+    
     include("php/util.php");
-    $header = logStatus();
-    session_start();
-
+    include("php/header.php");
     include("php/database.php");
-    // $db = DbConn('userInfo'); // DB接続
-    $db = DbConn('momo115_atto_demo', 'mysql57.momo115.sakura.ne.jp', 'momo115', 'atto_demo9'); // さくらDB接続
-    $emails = fldArray('email', 'loginProfile', $db);
-    $frozEmail = fldArray('email', 'frozenAccounts', $db);
+    $header = logStatus();
+    
+    $secretKey = md5(uniqid(mt_rand(), true));
+    $url = 'https://momo115.sakura.ne.jp/atto_php/reset.php?key=';
+    $url .= $secretKey;
+    if (isset($_POST['email'])){
+        $mail = new PHPMailer(true);
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'kenta.aratani5011@gmail.com';
+        $mail->Password = 'wtditjbdmjrimdsk';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+        $mail->setFrom('kenta.aratani5011@gmail.com', 'Kenta');
+        $mail->addAddress($_POST['email']);
+        $mail->Subject = 'Atto: Password Reset Link';
+        $mail->Body = "Please reset your password from below. The link is only active for the next 10 minutes.\n".$url;
+        try {
+            $mail->send();
+        } catch (Exception $e){
+            exit('PHPMailer Error: '.$mail->ErrorInfo);
+        }
+    }
 
-    if (in_array($_POST['email'], $emails) || in_array($_POST['email'], $frozEmail)){ // メール確認   
-        $url = 'https://momo115.sakura.ne.jp/atto_php/reset.php?key='; // デフォルト
-        $secretKey = md5(uniqid(mt_rand(), true));
-        $url .= $secretKey;
-        $title = 'Atto: password reset';
-        $content = "Please reset your password from below. The link is only active for the next 10 minutes.\n " . $url;
-        mail($_POST['email'], $title, $content); // パスワード再登録用メール送信
-        
+    $db = DbConn('userInfo');
+    // $db = DbConn();
+    $emails = array_merge(fldArray('email', 'loginCred', $db), fldArray('email', 'frozenAcct', $db));
+    if (in_array($_POST['email'], $emails)){   
         mkTbIF('token', 'email VARCHAR(256),tokenid VARCHAR(256),expires INT(10)', $db);
         $_POST['tokenid'] = $secretKey;
         $_POST['expires'] = strtotime("+ 10 minutes");
-        addData('token', 'email,tokenid,expires', $db, $_POST); // データ登録
-    }
+        addData('token', 'email,tokenid,expires', $db, $_POST);
+    }            
 ?>
 <html lang="en">
-<head>
+    <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
